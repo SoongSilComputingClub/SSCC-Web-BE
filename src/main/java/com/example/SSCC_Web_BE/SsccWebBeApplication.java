a59f3baa28030093ac05a99efb96c5b2e3ec0473
junekyu02 <baekjunekyu02@gmail.com>
2026-01-12 15:34:05 +0900

    [CHORE] 프로젝트 초기 설정 및 이슈,PR 템플릿 추가

---
+++ src/main/java/com/example/SSCC_Web_BE/SsccWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSCC_Web_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsccWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsccWebBeApplication.class, args);
+	}
+
+}
