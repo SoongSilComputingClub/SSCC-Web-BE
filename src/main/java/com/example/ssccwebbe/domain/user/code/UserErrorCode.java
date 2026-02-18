8a74cf3802caaae9e9bddb7220b8a91da4f1def1
bell-person-ii <leeji88460@gmail.com>
2026-02-19 01:58:32 +0900

    [Refactor] UserErrorCode 404 메시지 수정

--- src/main/java/com/example/ssccwebbe/domain/user/code/UserErrorCode.java
+++ src/main/java/com/example/ssccwebbe/domain/user/code/UserErrorCode.java
@@ -11,8 +11,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    // PreUser 관련 404 NOT_FOUND 에러
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "PREUSER4041", "해당 유저를 찾을 수 없습니다.");
+    // User 관련 404 NOT_FOUND 에러
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER4041", "해당 유저를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
