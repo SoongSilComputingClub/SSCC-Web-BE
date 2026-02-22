ead25f5b43a66b17b7593efe6370fd3a6a0171df
bell-person-ii <leeji88460@gmail.com>
2026-02-22 21:26:19 +0900

    [Hot-Fix] @JsonProperty 제거

--- src/main/java/com/example/ssccwebbe/domain/applyform/entity/Gender.java
+++ src/main/java/com/example/ssccwebbe/domain/applyform/entity/Gender.java
@@ -3,8 +3,6 @@ package com.example.ssccwebbe.domain.applyform.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum Gender {
-    @JsonProperty("male")
     MALE,
-    @JsonProperty("female")
     FEMALE
 }
